86ea31ded6e0feb78aec6b7f0055517e1cfdd238
abdullashahzan <abdullah.getstac@gmail.com>
2023-02-19 20:35:40 +0300

    v1.3

--- russian-roulette/static/script.js
+++ russian-roulette/static/script.js
@@ -7,29 +7,39 @@ const btn6 = document.querySelector('.btn6')
 const msg = document.querySelector('.lead')
 const main = document.querySelector('main')
 const allbtn = document.querySelector('.btn')
-const restartbutton = document.querySelector('.rbtn')
+const trybutton = document.querySelector('.tbtn')
 var numbers = [1, 2, 3, 4, 5, 6]
-const score = document.querySelector('.scores')
-var scores = 0
+const Try = document.querySelector('.scores')
+var tries;
+
+if (localStorage.getItem("tries") !== null) {
+    tries = parseInt(localStorage.getItem("tries"));
+} else {
+    tries = 1;
+}
+
+if (tries != 1){
+    Try.innerHTML = tries;
+}
 
 function check(n) {
-    var roullete = Math.floor(Math.random()*numbers.length);
+    var roullete = Math.floor(Math.random() * numbers.length);
     var roulletenumber = numbers[roullete];
     if (n == roulletenumber) {
-        msg.innerHTML = "You chose the wrong number... You died!";
+        msg.innerHTML = "You were killed 💀";
+        tries += 1
         main.style.backgroundColor = "gray"
         main.style.color = "white"
-        restartbutton.style.backgroundColor = "white"
-        restartbutton.style.color = "black"
-        restartbutton.style.border = "white"
+        trybutton.style.backgroundColor = "white"
+        trybutton.style.color = "black"
+        trybutton.style.border = "white"
         document.querySelector(".lostgif").style.display = 'block'
+        trybutton.style.display = 'block';
         restart()
     } else {
-        scores += 100
-        score.innerHTML = scores;
         if (roulletenumber == 1) {
             var index = numbers.indexOf(1);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn1.disabled = true;
             btn1.style.background = 'black'
             btn1.style.border = 'black'
@@ -39,7 +49,7 @@ function check(n) {
         }
         if (roulletenumber == 2) {
             var index = numbers.indexOf(2);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn2.disabled = true;
             btn2.style.background = 'black'
             btn2.style.border = 'black'
@@ -49,7 +59,7 @@ function check(n) {
         }
         if (roulletenumber == 3) {
             var index = numbers.indexOf(3);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn3.disabled = true;
             btn3.style.background = 'black'
             btn3.style.border = 'black'
@@ -59,7 +69,7 @@ function check(n) {
         }
         if (roulletenumber == 4) {
             var index = numbers.indexOf(4);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn4.disabled = true;
             btn4.style.background = 'black'
             btn4.style.border = 'black'
@@ -69,7 +79,7 @@ function check(n) {
         }
         if (roulletenumber == 5) {
             var index = numbers.indexOf(5);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn5.disabled = true;
             btn5.style.background = 'black'
             btn5.style.border = 'black'
@@ -79,7 +89,7 @@ function check(n) {
         }
         if (roulletenumber == 6) {
             var index = numbers.indexOf(6);
-            numbers.splice(index,1);
+            numbers.splice(index, 1);
             btn6.disabled = true;
             btn6.style.background = 'black'
             btn6.style.border = 'black'
@@ -87,21 +97,29 @@ function check(n) {
             btn6.classList.add('animate')
             msg.innerHTML = "Number 6 was killed";
         }
-    } if (numbers.length === 1){
+    } if (numbers.length === 1) {
+        tries = 0
         msg.innerHTML = "Congratulations you won!!";
         main.style.background = "#d4af37"
         main.style.color = "white"
-        restartbutton.style.backgroundColor = "white"
-        restartbutton.style.color = "black"
-        restartbutton.style.border = "white"
-        restartbutton.innerHTML = "Play again"
+        trybutton.style.backgroundColor = "white"
+        trybutton.style.color = "black"
+        trybutton.style.border = "white"
+        trybutton.innerHTML = "Play again"
+        trybutton.style.display = 'block';
         document.querySelector(".gifdiv").style.display = "block"
         restart()
     }
-        
+
 }
 
-function restart(){
+function tryagain() {
+    Try.innerHTML = tries
+    localStorage.setItem("tries", tries);
+    location.reload()
+}
+
+function restart() {
     btn1.disabled = true;
     btn2.disabled = true;
     btn3.disabled = true;
