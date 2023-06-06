346b1e7a88f509c41c9d9ecfa75285fdf65884cb
abdullashahzan <abdullah.getstac@gmail.com>
2023-06-06 23:14:53 +0300

    Commit via GitHub Desktop

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 100;
+        var elementVisible = 10;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
@@ -16,3 +16,31 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
+var navbtn = document.querySelector(".burger");
+var nav = document.querySelector(".overlayNav");
+var crossbtn = document.querySelector(".cross");
+
+navbtn.addEventListener("click", openMenu);
+crossbtn.addEventListener("click", closeMenu);
+
+function openMenu() {
+    nav.style.display = 'block';
+}
+
+function closeMenu() {
+    nav.style.display = "none";
+    nav.classList.remove = 'smooth'
+}
+
+
+var blurredContainer = document.querySelector(".getBlurred");
+
+function getBlurred() {
+    var blur;
+    if (window.pageYOffset < 200) {
+        blur = window.pageYOffset/80;
+    }
+    blurredContainer.style.filter = "blur(" + blur + "px)";
+}
+
+window.addEventListener("scroll", getBlurred);
