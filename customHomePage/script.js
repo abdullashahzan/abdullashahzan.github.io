5b65ce940408903373c414751c24be3ab3c7d0b9
kidneybeans123 <abdullah.shahzan@gmail.com>
2023-08-12 05:24:29 +0300

    new version

--- customHomePage/script.js
+++ customHomePage/script.js
@@ -1,5 +1,6 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
+const TodaysDate = document.querySelector(".date");
 const overlay = document.querySelector(".overlayContainer");
 const overlayIcon = document.querySelector(".overlayIcon");
 const timeField = document.querySelector(".timeField");
@@ -16,6 +17,34 @@ function setTime() {
     hour.innerHTML = formattedHours;
     minute.innerHTML = formattedMinutes;
 }
+
+function getFormattedDate(date) {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    let daySuffix;
+    if (day === 1 || day === 21 || day === 31) {
+        daySuffix = "st";
+    } else if (day === 2 || day === 22) {
+        daySuffix = "nd";
+    } else if (day === 3 || day === 23) {
+        daySuffix = "rd";
+    } else {
+        daySuffix = "th";
+    }
+
+    TodaysDate.innerHTML = `${dayOfWeek}, ${day}${daySuffix} ${month} ${year}`;
+}
+
+// Usage example:
+const date = new Date("2023-08-18");
+const formattedDate = getFormattedDate(date);
+console.log(formattedDate);
 
 function openCloseOverlay() {
     if (overlayStatus == 0) {
@@ -35,4 +64,4 @@ function openCloseOverlay() {
     }
 }
 
-setInterval(setTime, 5000);
+setInterval(setTime, 100);
