c9c57c7aec6ba17505bf242bd6e7dae79ee545d8
kidneybeans123 <abdullah.shahzan@gmail.com>
2023-08-12 04:28:06 +0300

    something bro

--- customHomePage/script.js
+++ customHomePage/script.js
@@ -1,5 +1,9 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
+const overlay = document.querySelector(".overlayContainer");
+const overlayIcon = document.querySelector(".overlayIcon");
+const timeField = document.querySelector(".timeField");
+var overlayStatus = 0;
 
 function setTime() {
     const hourAPI = new Date();
@@ -10,6 +14,22 @@ function setTime() {
 
     hour.innerHTML = formattedHours;
     minute.innerHTML = formattedMinutes;
+}
+
+function openCloseOverlay() {
+    if (overlayStatus == 0) {
+        overlayStatus = 1;
+        overlay.style.opacity = 1;
+        overlayIcon.classList.remove("bi-search");
+        overlayIcon.classList.add("bi-x-circle");
+        timeField.style.filter = "blur(10px)";
+    } else {
+        overlayStatus = 0;
+        overlay.style.opacity = 0
+        overlayIcon.classList.remove("bi-x-circle");
+        overlayIcon.classList.add("bi-search");
+        timeField.style.filter = "blur(0px)";
+    }
 }
 
 setInterval(setTime, 5000);
